970095f639bfdfabe866ec8c28e89e54ecbac543
iglika88 <51300608+iglika88@users.noreply.github.com>
2024-09-27 09:50:40 +0200

    Add files via upload

---
+++ lang/en/vocabpractice.php
@@ -0,0 +1,27 @@
+<?php
+// General plugin strings.
+$string['modulename'] = 'Vocabulary Practice';
+$string['modulenameplural'] = 'Vocabulary Practices';
+$string['pluginname'] = 'Vocabulary Practice';
+$string['pluginadministration'] = 'Vocabulary Practice Administration';
+
+// Capability strings.
+$string['vocabpractice:addinstance'] = 'Add a new Vocabulary Practice activity';
+$string['vocabpractice:view'] = 'View Vocabulary Practice activity';
+
+// Activity type strings.
+$string['activitytype'] = 'Activity Type'; // Label for the dropdown in mod_form.php
+$string['view'] = 'View Vocabulary List';
+$string['practice'] = 'Practice Vocabulary';
+$string['viewvocablist'] = 'View Vocabulary List'; // For 'activitytype' selection in mod_form.php
+
+// Help and information strings.
+$string['modulename_help'] = 'This activity allows you to practice vocabulary items with various exercises.';
+$string['vocabpractice_help'] = 'Vocabulary Practice allows users to engage in gap-fill and multiple-choice exercises to improve their vocabulary.';
+
+// Additional strings for your plugin.
+$string['vocabpractice:viewvocablist'] = 'View the list of vocabulary items';
+$string['vocabpractice:practiceactivity'] = 'Engage in vocabulary practice activities';
+$string['vocabpractice:manage'] = 'Manage vocabulary practice settings';
+$string['maximumchars'] = 'The maximum number of characters allowed is {$a}';
+
